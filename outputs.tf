233620400756550d6fd2778c9cfc5e7afd448ccb
tharaknad <93000414+tharaknad@users.noreply.github.com>
2025-09-05 07:40:10 +0530

    OutPut Format Change

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
-output "all_instance_ids" {
-  value = module.ec2-server.instance_ids
-}
-
-# Root outputs.tf
-output "public_ip" {
-  description = "Public IP(s) of the EC2 instance(s)"
-  value       = module.ec2-server.public_ip
-}
-
-output "private_ip" {
-  description = "Private IP(s) of the EC2 instance(s)"
-  value       = module.ec2-server.private_ip
+output "instances" {
+  description = "EC2 instance details from the ec2-server module"
+  value       = module.ec2-server.instances
 }
