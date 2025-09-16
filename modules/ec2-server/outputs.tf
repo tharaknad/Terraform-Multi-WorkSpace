ab61a3370218560595e637f365004598ed389c6d
Tharak Nad <93000414+tharaknad@users.noreply.github.com>
2025-09-16 06:09:25 +0530

    Bug-Fixes

--- modules/ec2-server/outputs.tf
+++ modules/ec2-server/outputs.tf
@@ -2,7 +2,7 @@ output "instances" {
   description = "Detailed info for each EC2 instance"
   value = {
     for idx, inst in aws_instance.this :
-    "pre-prod-ec2-${idx + 1}" => {
+    "${terraform.workspace}-ec2-${idx + 1}" => {
       id         = replace(inst.id, "i-", "")
       public_ip  = inst.public_ip
       private_ip = inst.private_ip
